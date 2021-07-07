87fab56fe9a09fabd621f2d9601114a6a2373d81
onnur0798 <onnur0798@gmail.com>
2021-07-07 07:24:23 +0530

    added values

---
+++ pratice/ec2.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "MyFirstEc2instance_from_terraform" {
+  ami = "ami-0b898040803850657"
+  instance_type = "t2.micro"
+  tags = {
+      Name = "EC2started_from_console_from_terraform"
+  }
+  key_name = "life"
+  user_data = <<-EOF
+                #!/bin/bash
+                yum update -y
+                yum install -y httpd
+                systemctl start httpd.service
+                systemctl enable httpd.service
+                echo "Hi Friend , I am $(hostname -f) hosted by Terraform" > /var/www/html/index.html
+                EOF
+}
